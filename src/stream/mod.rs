9cadde0790d3acc90bbf9b09e5317761310b1f67
PMK744 <69542280+PMK744@users.noreply.github.com>
2023-11-06 13:42:35 -0600

    added read/write uint24

--- src/stream/mod.rs
+++ src/stream/mod.rs
@@ -380,6 +380,35 @@ impl BinaryStream {
 }
 
 #[napi]
+// Unsigned 24-bit integer
+impl BinaryStream {
+  /**
+   * Reads an unsigned 24-bit ( 3 bytes ) integer to the stream. ( 0 to 16777215 )
+  */
+  #[napi]
+  pub fn read_uint24(&mut self, endian: Option<Endianness>) -> Result<u32> {
+    let endian = endian.unwrap_or(Endianness::Big);
+    let bytes = self.read(3)?;
+    match endian {
+      Endianness::Big => Ok(u32::from_be_bytes([0, bytes[0], bytes[1], bytes[2]])),
+      Endianness::Little => Ok(u32::from_le_bytes([0, bytes[0], bytes[1], bytes[2]])),
+    }
+  }
+
+  /**
+   * Writes an unsigned 24-bit ( 3 bytes ) integer to the stream. ( 0 to 16777215 )
+  */
+  #[napi]
+  pub fn write_uint24(&mut self, data: u32, endian: Option<Endianness>) -> Result<()> {
+    let endian = endian.unwrap_or(Endianness::Big);
+    match endian {
+      Endianness::Big => self.write(data.to_be_bytes()[1..].to_vec()),
+      Endianness::Little => self.write(data.to_le_bytes()[1..].to_vec()),
+    }
+  }
+}
+
+#[napi]
 // Unsigned 32-bit integer
 impl BinaryStream {
   /**
