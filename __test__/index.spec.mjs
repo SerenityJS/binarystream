6f6805bf3013ad90342a0da2339c89e979bbaa54
PMK744 <paytonmk744@outlook.com>
2024-03-05 12:03:48 -0600

    Proper stream testing

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,5 +1,13 @@
+import test from 'ava'
+
 import { BinaryStream } from '../index.js'
 
-const stream = new BinaryStream()
+test('sum from native', (t) => {
+  const stream = new BinaryStream()
 
-stream.writeString16('Hello, World!')
+  stream.writeVarString('Hello, World!')
+
+  const word = stream.readVarString()
+
+  t.is(word, 'Hello, World!')
+})
