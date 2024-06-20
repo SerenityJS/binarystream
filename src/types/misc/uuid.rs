1edaee3b801db083392915d29709764fa2ed5314
MisledWater079 <59891258+Misledwater79@users.noreply.github.com>
2024-06-20 00:46:26 -0400

    Updated UUID Format

--- src/types/misc/uuid.rs
+++ src/types/misc/uuid.rs
@@ -1,6 +1,6 @@
 use napi_derive::napi;
 use napi::Result;
-use crate::binary::BinaryStream;
+use crate::{binary::BinaryStream};
 
 #[napi]
 /**
@@ -19,12 +19,26 @@ impl Uuid {
    * Reads a signed 128-bit ( 16 bytes ) uuid string from the stream.
   */
   pub fn read(stream: &mut BinaryStream) -> Result<String> {
-    let bytes = match stream.read(16) {
+    let mut bytes = match stream.read(8) {
       Ok(bytes) => bytes,
       Err(err) => return Err(err)
     };
 
+    //bytes.reverse();
+
     let mut uuid = String::new();
+
+    for byte in bytes {
+      uuid.push_str(&format!("{:02X}", byte));
+    }
+
+    bytes = match stream.read(8) {
+      Ok(bytes) => bytes,
+      Err(err) => return Err(err)
+    };
+    
+    //bytes.reverse();
+    
     for byte in bytes {
       uuid.push_str(&format!("{:02X}", byte));
     }
@@ -42,10 +56,23 @@ impl Uuid {
     let mut data = value.replace("-", "");
     data = data.to_uppercase();
     let mut bytes = Vec::new();
-    for i in 0..16 {
+    for i in 0..8 {
       let byte = u8::from_str_radix(&data[i * 2..i * 2 + 2], 16).unwrap();
       bytes.push(byte);
     }
+
+    bytes.reverse();
+    
+    stream.write(bytes);
+    
+    bytes = Vec::new();
+    for i in 8..16 {
+      let byte = u8::from_str_radix(&data[i * 2..i * 2 + 2], 16).unwrap();
+      bytes.push(byte);
+    }
+
+    bytes.reverse();
+    
     stream.write(bytes)
   }
 }
